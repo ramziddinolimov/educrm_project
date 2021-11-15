394433de9d17dc8a30bcae74a10581d5ab990472
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-15 22:13:53 +0500

    Create ApplicantController.js

---
+++ src/controllers/ApplicantController.js
@@ -0,0 +1,4 @@
+const permissionChecker = require('../helpers/permissionChecker');
+const { AddApplicantValidation, UpdateApplicantValidation } = require('../modules/validations')
+
+module.exports = class AddApplicantController {}
