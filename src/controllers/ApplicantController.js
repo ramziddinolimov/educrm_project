fa3c4554f260dbbea25ff935e0d2916e32d971d6
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-15 22:25:52 +0500

    static async ApplicantGetController

--- src/controllers/ApplicantController.js
+++ src/controllers/ApplicantController.js
@@ -1,4 +1,38 @@
 const permissionChecker = require('../helpers/permissionChecker');
 const { AddApplicantValidation, UpdateApplicantValidation } = require('../modules/validations')
 
-module.exports = class AddApplicantController {}
+module.exports = class AddApplicantController {
+    static async ApplicantGetController(req, res, next) {
+        try {
+            permissionChecker(
+                ["admin", "operator"],
+                req.user_permissions,
+                res.error
+            );
+
+            const limit = req.query.limit || 15;
+            const offset = req.query.offset - 1 || 0;
+
+            const applicants = await req.db.applicants.findAll({ 
+                limit,
+                offset: offset * 15,
+                include: [
+                    {
+                        model: req.db.users,
+                    },
+                    {
+                        model: req.db.courses,
+                    },
+                ],
+            });
+
+            res.json({
+                ok: true,
+                message: "OK",
+                data: {applicants,},
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
