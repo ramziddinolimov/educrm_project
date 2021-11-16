d8641cf25cefe295b2f3b44b72b3c7f7b0fdd2ad
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-16 21:49:24 +0500

    course model finished

--- src/models/CourseModel.js
+++ src/models/CourseModel.js
@@ -10,6 +10,18 @@ module.exports = async (sequelize, Sequelize) => {
             type: Sequelize.STRING(64),
             allowNull: false,
         },
-        
-    })
-}
+        course_price: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        course_description: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+        course_photo: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+    });
+};
